ef159ef4d823a3c4e62cbb0863f28e6c0a89fb2d
Sharavana Kumar S <132196939+sharavanask@users.noreply.github.com>
2025-07-11 14:18:17 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
+from pydantic import BaseModel
 import psq
 
 app = FastAPI()
+
+class QueryInput(BaseModel):
+    query: str
 
 @app.get("/")
 async def root():
@@ -11,13 +15,12 @@ async def root():
 async def list_databases():
     return await psq.list_databases()
 
-
 @app.get("/list_tables")
 async def list_tables():
     return await psq.list_tables()
 
 @app.get("/get_table_info")
-async def get_table_info(table_name: str = Query(...)):
+async def get_table_info(table_name: str):
     return await psq.get_table_info(table_name)
 
 @app.get("/get_relationships")
@@ -25,9 +28,8 @@ async def get_relationships():
     return await psq.get_relationships()
 
 @app.post("/execute_query")
-async def execute_query(query: str = Query(...)):
-    return await psq.execute_query(query) 
-
+async def execute_query(input: QueryInput):
+    return await psq.execute_query(input)
 
 if __name__ == "__main__":
     import uvicorn
